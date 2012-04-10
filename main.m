1e2fb064f7c9290da1b9fe1f419e429458a0107c
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-04-10 10:04:35 +0200

    Finished testing on ARM (it works)

--- main.m
+++ main.m
@@ -1,21 +1,8 @@
-//
-//  main.m
-//  objcswitch_ios_tests
-//
-//  Created by Nicolas Bouilleaud on 10/04/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#import "TargetConditionals.h"
-#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
-#endif
 
 int main(int argc, char *argv[])
 {
-#if TARGET_OS_IPHONE
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, nil);
     }
-#endif
 }

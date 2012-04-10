9d430fc45b98c00f39a5287e935dd38e2268385e
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-04-10 09:42:13 +0200

    ARM/iOS test target, continued.

--- objcswitch_ios_tests/main.m
+++ objcswitch_ios_tests/main.m
@@ -6,11 +6,16 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#import "TargetConditionals.h"
+#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#endif
 
 int main(int argc, char *argv[])
 {
+#if TARGET_OS_IPHONE
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, nil);
     }
+#endif
 }
